3609b85e81cd74764b8769ec51b8cd12a7eb094a
unknown <nguyenquocduong1983@gmail.com>
2018-09-12 16:37:36 +0700

    chapter3

---
+++ Chapter/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage ="Invaid Email")]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool ? WillAttend { get; set; }
+
+    }
+}
